b62aa09d7127ae569d3cf5fbc947aca6fbc088d0
Xav-Sathorn <x.coenen.dev@gmail.com>
2022-09-28 11:22:04 +0200

    03-path twig ok

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,9 +3,10 @@
 namespace App\Controller;
 
 use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
 
 class ProductController extends AbstractController
 {
@@ -19,11 +20,33 @@ class ProductController extends AbstractController
             'slug' => $slug
         ]);
 
-        dd($category);
+        if (!$category) {
+            throw $this->createNotFoundException("La catégorie demandée n'existe pas !");
+        }
 
 
         return $this->render('product/category.html.twig', [
             'slug' => $slug,
+            'category' => $category
+        ]);
+    }
+
+    /**
+     * @Route("/{category_slug}/{slug}", name="product_show")
+     */
+
+    public function show($slug, ProductRepository $productRepository)
+    {
+
+        $product = $productRepository->findOneBy([
+            'slug' => $slug
+        ]);
+
+        if (!$product) {
+            throw $this->createNotFoundException("Le produit demandée n'existe pas !");
+        }
+        return $this->render('product/show.html.twig', [
+            'product' => $product
         ]);
     }
 }
